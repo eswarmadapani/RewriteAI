3d73d76bb0273599bc215e3ecdde479724b186f7
eswar <easravan4@gmail.com>
2026-02-08 11:48:24 +0530

    Fix case-sensitive import path for Landing component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/landing";
+import Landing from "./pages/Landing";
 import Rewrite from "./pages/Rewrite";
 import "./App.css";
 
